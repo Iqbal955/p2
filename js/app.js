0c3a3532874e0a1e58cbc4e17db8f5b3d180502f
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-02-16 11:41:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const list = document.querySelectorAll(".student-item");
 
 const page = document.querySelectorAll(".page");
+const maxprPage = 10;
 
 const showPage = (list, page) => {
   
@@ -9,39 +10,41 @@ const showPage = (list, page) => {
   
  for(i=0;i<list.length; i++) {
     
-    if(i >= 0 && i <= 10) {
+    if(i >= startIndex && i <= endIndex) {
       
       list[i].style.display = "";
       
     }
    
-    else if(i >= startIndex && i <= endIndex) {
+   else{
+   
+
       
       list[i].style.display = "none";
       
     }
   }
   
-
+ 
+  //How would I call to test the function?
   
 }
-showPage(list, 1);
 
 
 
 
-const appendPageLinks = (links) => {
+const appendPageLinks = (list) => {
   
   
-  const pageNumbers = page.length/10; //Dividing the page.length with the max amount items pr page
-  const div = document.createElement(div); //creating the div element
-  div.setAttribute("class", "“pagination”"); //setting the class to pagination
+  const pageNumbers = page.length/maxprPage; //Dividing the page.length with the max amount items pr page
+  const div = document.createElement("div"); //creating the div element
+  div.setAttribute("class", "pagination"); //setting the class to pagination
   div.appendChild(page); //append page to div
   
   
   const ul = document.createElement(ul); //creating the ul element
   
-         ul.appendChild(div); //append div to li
+  ul.appendChild(div); //append div to li
   
   
   
@@ -49,19 +52,17 @@ const appendPageLinks = (links) => {
             
           const li = document.createElement("li"); //create every li element
           const a = document.createElement("a"); //create an a tag element
-                 li.appendChild(a); //append li to the a element
-                 a.textContent(i); //setting the text content of a to the i'th number
-                 li.textContent(i); 
-         
+          li.appendChild(a); //append li to the a element
+          a.textContent(i); //setting the text content of a to the i'th number
+    
+          
 
    
-  
-
   for (j=0; j<a.length; j++) { //for every a tag
-    a.addEventListener("click", "");  //add an eventlistner when ever it is pressed
+          a.addEventListener("click", (e));  //add an eventlistner when ever it is pressed
     
-  const divLength =  e.target.className = "active"; //target the active class and removing it
-  divLength.removeClass;
+          const divLength =  e.target.className = "active"; //target the active class and removing it
+          divLength.removeClass;
     
     
     
@@ -70,12 +71,16 @@ const appendPageLinks = (links) => {
 // The active class name should be added to the link that was just clicked. The target property of the event object should be useful here. ? 
    //   The function to show a page should be called, passing in as arguments, the global variable for the list items, and the page number that should be shown. The text content of the A element that was just clicked can be helpful here.
   
+    
+    
+    
       
     
   }
 }
   } 
     
- 
+ showPage(list, 1);
+appendPageLinks(list);
   
 
